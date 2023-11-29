f9dfa6987a7bae6c014679dc0395fad8cef5f084
kevindunbar00 <kevin@velocode.ai>
2023-11-29 15:53:23 +0200

    Standardize date formats to ISO 8601

--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -1,24 +1,27 @@
 // Sample JavaScript code for a made-up application
-// Function to display user details
+// Function to display user details with standardized date formats
+
+function standardizeDateFormat(dateStr) {
+  // Logic to convert date to ISO format (YYYY-MM-DD)
+  const [month, day, year] = dateStr.split(/\/|-/);
+  return `${year}-${month}-${day}`;
+}
 
 function displayUserDetails(user) {
-    // Date in MM/DD/YYYY format
-    let signUpDate = user.signUpDate; // e.g., "12/31/2021"
-  
-    // Date in DD-MM-YYYY format
-    let lastLoginDate = user.lastLoginDate; // e.g., "31-12-2021"
-  
-    console.log(`User: ${user.name}`);
-    console.log(`Signed up on: ${signUpDate}`);
-    console.log(`Last login: ${lastLoginDate}`);
-  }
-  
-  // Example user object
-  const user = {
-    name: "Jane Doe",
-    signUpDate: "12/31/2021",
-    lastLoginDate: "31-12-2021"
-  };
-  
-  displayUserDetails(user);
-  
+  // Convert dates to ISO format
+  let signUpDate = standardizeDateFormat(user.signUpDate); // "2021-12-31"
+  let lastLoginDate = standardizeDateFormat(user.lastLoginDate); // "2021-12-31"
+
+  console.log(`User: ${user.name}`);
+  console.log(`Signed up on (ISO Format): ${signUpDate}`);
+  console.log(`Last login (ISO Format): ${lastLoginDate}`);
+}
+
+// Example user object with original date formats
+const user = {
+  name: "Jane Doe",
+  signUpDate: "12/31/2021",
+  lastLoginDate: "31-12-2021"
+};
+
+displayUserDetails(user);
